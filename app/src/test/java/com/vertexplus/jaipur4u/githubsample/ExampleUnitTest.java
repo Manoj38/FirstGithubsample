b1f8dcca19256da4efecbd2a03b4002d7407c28e
Manoj Godara <manoj.godara@vertexplus.inet>
2016-02-11 15:29:52 +0530

    Initial commit

---
+++ app/src/test/java/com/vertexplus/jaipur4u/githubsample/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.vertexplus.jaipur4u.githubsample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
